c339d00a5144dae83df91f4ac32d2f220e614d4f
GAKiknadze <gakiknadze@yandex.ru>
2025-03-31 03:13:14 +0300

    Удален метод создания пользователей

--- src/services/user.py
+++ src/services/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..models.user import User, UserRole
-from ..schemas.user import UserCreate, UserUpdate
+from ..schemas.user import UserUpdate
 from .exceptions import BadRequestExc, ObjectNotFoundExc, SomethingWrongExc
 
 logger = logging.getLogger(__name__)
@@ -41,28 +41,28 @@ class UserService:
         result = await db.execute(query)
         return result.scalars().first()
 
-    @staticmethod
-    async def create(db: AsyncSession, user_data: UserCreate) -> User:
-        user = User(
-            email=user_data.email,
-            login=user_data.login,
-            name=user_data.name,
-            role=UserRole.CLIENT,
-        )
+    # @staticmethod
+    # async def create(db: AsyncSession, user_data: UserCreate) -> User:
+    #     user = User(
+    #         email=user_data.email,
+    #         login=user_data.login,
+    #         name=user_data.name,
+    #         role=UserRole.CLIENT,
+    #     )
 
-        try:
-            db.add(user)
-            await db.commit()
-            await db.refresh(user)
-            return user
-        except IntegrityError as e:
-            await db.rollback()
-            logger.error(f"Can`t create account: {e}")
-            if "email" in str(e):
-                logger.debug(f"Email {user_data.email} already registered")
-                raise BadRequestExc("Email already registered")
-            logger.debug("User creation failed")
-            raise BadRequestExc("User creation failed")
+    #     try:
+    #         db.add(user)
+    #         await db.commit()
+    #         await db.refresh(user)
+    #         return user
+    #     except IntegrityError as e:
+    #         await db.rollback()
+    #         logger.error(f"Can`t create account: {e}")
+    #         if "email" in str(e):
+    #             logger.debug(f"Email {user_data.email} already registered")
+    #             raise BadRequestExc("Email already registered")
+    #         logger.debug("User creation failed")
+    #         raise BadRequestExc("User creation failed")
 
     @staticmethod
     async def update_by_id(
